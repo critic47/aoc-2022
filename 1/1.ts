699b4233439f8fcb57022fcfee56f281cf9f3ed7
Lorenzo Abram <lorenzo.abram@gmail.com>
2022-12-01 13:34:55 +0100

    day 1

---
+++ 1/1.ts
@@ -0,0 +1,27 @@
+const input = (await Deno.readTextFile('./1.input.txt')).split('\n')
+
+const elves: number[] = []
+
+let currentElf = 0
+let max = 0
+let top3: [number, number, number] = [0, 0, 0]
+for(const v of input) {
+    const val = parseInt(v)
+    if (!isNaN(val)) {
+        elves[currentElf] = (elves[currentElf] || 0) + val
+    } else {
+        const [a, b, c] = top3
+        max = elves[currentElf] > max ? elves[currentElf] : max
+        if (elves[currentElf] > a) {
+            top3 = [elves[currentElf], a, b]
+        } else if (elves[currentElf] > b) {
+            top3 = [a, elves[currentElf], b]
+        } else if (elves[currentElf] > c) {
+            top3 = [a, b, elves[currentElf]] 
+        }
+        currentElf++
+    }
+}
+
+console.log('Max:', max)
+console.log('Top3 total:', top3.reduce((tot, n) => tot + n, 0))
