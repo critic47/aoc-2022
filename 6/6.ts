aad10bc8b7598b7c4e19ffdaa60864463cfaea0c
Lorenzo Abram <lorenzo.abram@gmail.com>
2022-12-06 15:09:31 +0100

    day 6 remove comments

--- 6/6.ts
+++ 6/6.ts
@@ -4,7 +4,6 @@ console.log(input)
 
 //part 1
 for (let i = 0; i < input.length-3; i++) {
-    console.log(input.substring(i, i+4))
     const s = new Set(input.substring(i, i+4))
     if (s.size === 4) {
         console.log(i+4)
@@ -14,7 +13,6 @@ for (let i = 0; i < input.length-3; i++) {
 
 //part 2
 for (let i = 0; i < input.length-13; i++) {
-    console.log(input.substring(i, i+14))
     const s = new Set(input.substring(i, i+14))
     if (s.size === 14) {
         console.log(i+14)
