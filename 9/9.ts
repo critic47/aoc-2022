02040f28b6d254d78b4239cab898d01f2b605d31
Lorenzo Abram <lorenzo.abram@gmail.com>
2023-10-17 01:19:17 +0200

    day 9

---
+++ 9/9.ts
@@ -0,0 +1,38 @@
+const input = (await Deno.readTextFile('./9/9.input.txt')).split('\n')
+
+type Coord = [number, number]
+type Move = ['U' | 'D' | 'L' | 'R', number]
+
+const head: Coord = [0, 0]
+const tail: Coord = [0, 0]
+const tailPositions = new Set<string>()
+tailPositions.add(`${tail[0]},${tail[1]}}`) // add starting position
+
+const distance = (a: Coord, b: Coord) => Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]))
+
+input.forEach((line) => {
+  const move = line.split(' ') as Move
+  for (let i = 0; i < move[1]; i++) {
+    switch (move[0]) {
+      case 'U':
+        head[1]++
+        break
+      case 'D':
+        head[1]--
+        break
+      case 'L':
+        head[0]--
+        break
+      case 'R':
+        head[0]++
+        break
+    }
+    if (distance(head, tail) > 1) {
+      tail[0] = tail[0] + (head[0] - tail[0]) / (Math.abs(head[0] - tail[0]) || 1)
+      tail[1] = tail[1] + (head[1] - tail[1]) / (Math.abs(head[1] - tail[1]) || 1)
+      tailPositions.add(`${tail[0]},${tail[1]}}`)
+    }
+  }
+})
+
+console.log(tailPositions.size)
---
+++ 9/9b.ts
@@ -0,0 +1,53 @@
+const input = (await Deno.readTextFile('./9/9.input.txt')).split('\n')
+
+type Coord = [number, number]
+type Move = ['U' | 'D' | 'L' | 'R', number]
+
+const rope: Coord[] = [
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+]
+const tailPositions = new Set<string>()
+tailPositions.add(`${rope[9][0]},${rope[9][1]}}`) // add starting position
+
+const distance = (a: Coord, b: Coord) => Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]))
+
+input.forEach((line) => {
+  const move = line.split(' ') as Move
+  const head = rope[0]
+  for (let i = 0; i < move[1]; i++) {
+    switch (move[0]) {
+      case 'U':
+        head[1]++
+        break
+      case 'D':
+        head[1]--
+        break
+      case 'L':
+        head[0]--
+        break
+      case 'R':
+        head[0]++
+        break
+    }
+    for (let i = 1; i < rope.length; i++) {
+      const prevKnot = rope[i - 1]
+      const knot = rope[i]
+      if (distance(prevKnot, knot) > 1) {
+        knot[0] = knot[0] + (prevKnot[0] - knot[0]) / (Math.abs(prevKnot[0] - knot[0]) || 1)
+        knot[1] = knot[1] + (prevKnot[1] - knot[1]) / (Math.abs(prevKnot[1] - knot[1]) || 1)
+      }
+    }
+    tailPositions.add(`${rope[9][0]},${rope[9][1]}}`) // add tail position
+  }
+})
+
+console.log(tailPositions.size)
